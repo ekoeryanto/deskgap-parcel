6e602d5c970f054461faa72cb8ef80565b32fe1b
Muhibbudin Suretno <muhibbudinsuretno1@gmail.com>
2019-07-01 21:30:27 +0700

    Update dependencies

--- main.js
+++ main.js
@@ -1,15 +1,12 @@
 const { ENV } = process.env
 const { app, BrowserWindow } = require('deskgap');
 
-const PARCEL_SERVE = 'http://localhost:1234'
-const PARCEL_BUILD = 'dist/index.html'
-
 app.once('ready', () => {
     const win = new BrowserWindow();
 
     if (ENV === 'development') {
-      win.loadURL(PARCEL_SERVE);
+      win.loadURL('http://localhost:1234');
     } else {
-      win.loadFile(PARCEL_BUILD);
+      win.loadFile('dist/index.html');
     }
 });
