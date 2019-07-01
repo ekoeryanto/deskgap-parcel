87ad51cb055e37d945ef0758be9ed0a426b9cfaa
Muhibbudin Suretno <muhibbudinsuretno1@gmail.com>
2019-07-01 21:14:16 +0700

    Move load file to const

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 const { ENV } = process.env
 const { app, BrowserWindow } = require('deskgap');
 
+const PARCEL_SERVE = 'http://localhost:1234'
+const PARCEL_BUILD = 'dist/index.html'
+
 app.once('ready', () => {
     const win = new BrowserWindow();
 
     if (ENV === 'development') {
-      win.loadURL('http://localhost:1234');
+      win.loadURL(PARCEL_SERVE);
     } else {
-      win.loadFile('dist/index.html');
+      win.loadFile(PARCEL_BUILD);
     }
 });
